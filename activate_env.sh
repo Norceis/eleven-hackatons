763a8fd15327562838a58c6cc4d6ff2434cbcafd
Jakub Kowalczewski <cubix77@gmail.com>
2025-02-22 17:12:52 +0100

    env changes

--- activate_env.sh
+++ activate_env.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ ! -e env/bin/activate ]; then
-  python3.10 -m venv env/ || return # exit on error
+  python3.12 -m venv env/ || return # exit on error
   source env/bin/activate
   echo "Activated virtual env"
   python -m pip install --upgrade pip wheel setuptools
   python -m pip install -e .[all]
+  python -m pip install -r requirements.txt
 else
   source env/bin/activate
   python -m pip install -e .[all]
+python -m pip install -r requirements.txt
   echo "Activated virtual env"
 fi
